130b0bc4e6ecb69341cf03c93cc7c95e357353ec
Marlon Alejandro Espinosa Castañeiras <marlon9402@outlook.com>
2025-11-28 15:10:46 -0500

    initial configuration of required pages

---
+++ app/(tabs)/favorites.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function FavoritesScreen() {
+  return (
+    <ThemedView>
+      <ThemedText>Shop</ThemedText>
+    </ThemedView>
+  );
+}
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,10 +2,10 @@ import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+export default function ProfileScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText>Home</ThemedText>
+      <ThemedText>Profile</ThemedText>
     </ThemedView>
   );
 }
